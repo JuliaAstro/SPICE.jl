5feb98e97ca9124b8b6d17bfeeba0f3b43d63dba
Helge Eichhorn <git@helgeeichhorn.de>
2016-02-09 13:19:10 +0100

    Fix date formatting for v0.5.

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,12 +14,12 @@ if !isfile(SPK_FILE)
     run(download_cmd(SPK_URL*SPK_FILE, SPK_FILE))
 end
 
-furnsh(["de430.bsp", "naif0011.tls"])
+furnsh([SPK_FILE, LSK_FILE])
 
-datestring0 = string(now())
+datestring0 = Dates.format(now(), "yyyy-mm-ddTHH:MM:SS")
 et = str2et(datestring0)
 datestring1 = timout(et,"YYYY-MM-DDTHR:MN:SC")
 @test datestring1 == datestring0
 @test_throws ErrorException timout(et,"")
 
-unload(["de430.bsp", "naif0011.tls"])
+unload([SPK_FILE, LSK_FILE])
