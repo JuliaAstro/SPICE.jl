9e30f8728afa75ba617d4b04269ca02d58722d52
Helge Eichhorn <git@helgeeichhorn.de>
2017-02-17 09:23:15 +0100

    Try to fix unix build.

--- deps/build.jl
+++ deps/build.jl
@@ -3,25 +3,23 @@ using BinDeps
 @BinDeps.setup
 
 cspice = library_dependency("libcspice")
-@osx_only provides(Sources, URI("https://naif.jpl.nasa.gov/pub/naif/toolkit/C/MacIntel_OSX_AppleC_$(WORD_SIZE)bit/packages/cspice.tar.Z"), cspice, os=:Darwin)
-@linux_only provides(Sources, URI("https://naif.jpl.nasa.gov/pub/naif/toolkit/C/PC_Linux_GCC_$(WORD_SIZE)bit/packages/cspice.tar.Z"), cspice, os=:Linux)
+provides(Sources, URI("http://naif.jpl.nasa.gov/pub/naif/toolkit/C/MacIntel_OSX_AppleC_64bit/packages/cspice.tar.Z"), cspice, os=:Darwin)
+provides(Sources, URI("http://naif.jpl.nasa.gov/pub/naif/toolkit/C/PC_Linux_GCC_$(Sys.WORD_SIZE)bit/packages/cspice.tar.Z"), cspice, os=:Linux)
 
-@windows_only provides(Binaries, URI("https://github.com/helgee/SPICE.jl/releases/download/N0065/libcspice$(WORD_SIZE).zip"), cspice, os=:Windows)
+provides(Binaries, URI("https://github.com/helgee/SPICE.jl/releases/download/N0065/libcspice$(Sys.WORD_SIZE).zip"), cspice, os=:Windows)
 
 prefix = joinpath(BinDeps.depsdir(cspice), "usr")
 build = joinpath(BinDeps.depsdir(cspice), "build")
 
-@unix_only begin
-    provides(SimpleBuild, (@build_steps begin
-        GetSources(cspice)
-        CreateDirectory(build)
-        @build_steps begin
-            ChangeDirectory(build)
-            `cmake -DCMAKE_INSTALL_PREFIX=$prefix ..`
-            `cmake --build .`
-            `cmake --build . --target install`
-        end
-    end), cspice, os=:Unix)
-end
+provides(SimpleBuild, (@build_steps begin
+    GetSources(cspice)
+    CreateDirectory(build)
+    @build_steps begin
+        ChangeDirectory(build)
+        `cmake -DCMAKE_INSTALL_PREFIX=$prefix ..`
+        `cmake --build .`
+        `cmake --build . --target install`
+    end
+end), cspice, os=:Unix)
 
 @BinDeps.install Dict(:libcspice => :libcspice)
