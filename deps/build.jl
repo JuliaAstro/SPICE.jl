beea34b883dcd0bd453a772087c65f7acfa4740c
Helge Eichhorn <git@helgeeichhorn.de>
2017-02-17 09:53:34 +0100

    Fix Travis.

--- deps/build.jl
+++ deps/build.jl
@@ -3,8 +3,8 @@ using BinDeps
 @BinDeps.setup
 
 cspice = library_dependency("libcspice")
-provides(Sources, URI("http://naif.jpl.nasa.gov/pub/naif/toolkit/C/MacIntel_OSX_AppleC_64bit/packages/cspice.tar.Z"), cspice, os=:Darwin)
-provides(Sources, URI("http://naif.jpl.nasa.gov/pub/naif/toolkit/C/PC_Linux_GCC_$(Sys.WORD_SIZE)bit/packages/cspice.tar.Z"), cspice, os=:Linux)
+provides(Sources, URI("https://naif.jpl.nasa.gov/pub/naif/toolkit/C/MacIntel_OSX_AppleC_64bit/packages/cspice.tar.Z"), cspice, os=:Darwin)
+provides(Sources, URI("https://naif.jpl.nasa.gov/pub/naif/toolkit/C/PC_Linux_GCC_$(Sys.WORD_SIZE)bit/packages/cspice.tar.Z"), cspice, os=:Linux)
 
 provides(Binaries, URI("https://github.com/helgee/SPICE.jl/releases/download/N0065/libcspice$(Sys.WORD_SIZE).zip"), cspice, os=:Windows)
 
