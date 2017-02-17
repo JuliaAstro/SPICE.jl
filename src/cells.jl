fcdc8dd00faf4d8ca819ae2c58ac078cac18482a
Helge Eichhorn <git@helgeeichhorn.de>
2017-02-17 16:16:23 +0100

    Document and properly test cells.

--- src/cells.jl
+++ src/cells.jl
@@ -1,5 +1,5 @@
-import Base: getindex, endof
-export SpiceIntCell, SpiceDoubleCell, SpiceCharCell, appnd
+import Base: getindex, endof, push!, append!, show
+export SpiceIntCell, SpiceDoubleCell, SpiceCharCell, appnd, push!, append!
 
 const CTRLSZ = 6
 
@@ -32,31 +32,31 @@ typealias SpiceDoubleCell SpiceCell{SpiceDouble,1}
 typealias SpiceIntCell SpiceCell{SpiceInt,1}
 
 endof(cell::SpiceCell) = cell.cell.card
+show{T}(io::IO, cell::SpiceCell{T,1}) = print(io, "SpiceCell{$(T.name.name)}($(cell.cell.size))")
+show{T}(io::IO, cell::SpiceCell{T,2}) = print(io, "SpiceCell{$(T.name.name)}($(cell.cell.size),$(cell.cell.length))")
 
 function check_ind(cell, inds)
     ind = collect(inds)[end]
     if ind > cell.cell.size
-        error("Index $ind is out of bounds. The size of this cell is $(cell.cell.size).")
+        error("Index $ind is out of bounds. The size of this SpiceCell is $(cell.cell.size).")
     elseif ind > cell.cell.card
         error("The element at index $ind is unassigned.")
     end
 end
 
-function getindex(cell::SpiceCell, ind)
-    check_ind(cell, ind)
-    getindex(cell, ind)
-end
-
 function getindex{T<:Real}(cell::SpiceCell{T}, ind)
+    check_ind(cell, ind)
     return cell.data[CTRLSZ + ind]
 end
 
 function getindex(cell::SpiceCharCell, ind::Int)
+    check_ind(cell, ind)
     str = cell.data[:, CTRLSZ + ind]
     return String(str[1:findfirst(str .== 0)-1])
 end
 
 function getindex(cell::SpiceCharCell, ind)
+    check_ind(cell, ind)
     str = cell.data[:, CTRLSZ + ind]
     return vec(mapslices(x -> String(x[1:findfirst(x .== 0)-1]), str, 1))
 end
@@ -73,6 +73,12 @@ function SpiceCell{SpiceChar}(::Type{SpiceChar}, size, length)
     self.cell.data = data
     return self
 end
+
+"""
+    SpiceCharCell(size, length)
+
+Create a SpiceCharCell that can contain up to `size` strings with `length` characters.
+"""
 SpiceCharCell(size::Int, length::Int) = SpiceCell(SpiceChar, size, length)
 
 function SpiceCell{T<:Real}(::Type{T}, size)
@@ -85,7 +91,19 @@ function SpiceCell{T<:Real}(::Type{T}, size)
     self.cell.data = data
     return self
 end
+
+"""
+    SpiceDoubleCell(size)
+
+Create a SpiceDoubleCell that can contain up to `size` elements.
+"""
 SpiceDoubleCell(size) = SpiceCell(SpiceDouble, size)
+
+"""
+    SpiceIntCell(size)
+
+Create a SpiceIntCell that can contain up to `size` elements.
+"""
 SpiceIntCell(size) = SpiceCell(SpiceInt, size)
 
 for (t, f) in zip((SpiceInt, SpiceDouble), ("appndi_c", "appndd_c"))
@@ -102,7 +120,7 @@ end
 """
     appnd(item, cell)
 
-Append an `item` to a char/double/integer `cell`.
+Append an `item` to the char/double/integer SpiceCell `cell`.
 
 [https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/appndc_c.html](https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/appndc_c.html)
 [https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/appndd_c.html](https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/appndd_c.html)
@@ -114,4 +132,18 @@ function appnd(item, cell::SpiceCell{SpiceChar})
     handleerror()
     return nothing
 end
+
+"""
+    push!(cell, items...)
+
+Insert one or more `items` at the end of the char/double/integer SpiceCell `cell`.
+"""
+push!(cell::SpiceCell, items...) = foreach(x -> appnd(x, cell), items)
+
+"""
+    append!(cell, collection)
+
+Append all items from `collection` to the char/double/integer SpiceCell `cell`.
+"""
+append!(cell::SpiceCell, collection) = foreach(x -> appnd(x, cell), collection)
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,5 @@
 using SPICE
 using Base.Test
-using BinDeps
 
 const LSK_URL = "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/"
 const LSK_FILE = "naif0012.tls"
@@ -16,34 +15,62 @@ if !isfile(SPK_FILE)
     download(SPK_URL*SPK_FILE, SPK_FILE)
 end
 
-furnsh([SPK_FILE, LSK_FILE])
+@testset "Time" begin
+    furnsh([SPK_FILE, LSK_FILE])
 
-datestring0 = Dates.format(now(), "yyyy-mm-ddTHH:MM:SS")
-et = str2et(datestring0)
-datestring1 = timout(et,"YYYY-MM-DDTHR:MN:SC")
-@test datestring1 == datestring0
-@test_throws ErrorException timout(et,"")
+    datestring0 = Dates.format(now(), "yyyy-mm-ddTHH:MM:SS")
+    et = str2et(datestring0)
+    datestring1 = timout(et,"YYYY-MM-DDTHR:MN:SC")
+    @test datestring1 == datestring0
+    @test_throws ErrorException timout(et,"")
 
-unload([SPK_FILE, LSK_FILE])
+    unload([SPK_FILE, LSK_FILE])
+end
 
-# isfile("test.spk") && rm("test.spk")
-# handle = spkopn("test.spk", "SPK_test", 0)
-# spkcls(handle)
+@testset "Cells" begin
+    cell = SpiceIntCell(3)
+    appnd(1, cell)
+    appnd(2, cell)
+    appnd(3, cell)
+    @test cell[1:end] == [1, 2, 3]
 
-cell = SpiceIntCell(3)
-appnd(1, cell)
-appnd(2, cell)
-appnd(3, cell)
-@test cell[1:3] == [1, 2, 3]
+    cell = SpiceIntCell(3)
+    push!(cell, 1, 2, 3)
+    @test cell[1:end] == [1, 2, 3]
 
-cell = SpiceDoubleCell(3)
-appnd(1, cell)
-appnd(2, cell)
-appnd(3, cell)
-@test cell[1:3] == [1.0, 2.0, 3.0]
+    cell = SpiceIntCell(3)
+    append!(cell, [1, 2, 3])
+    @test cell[1:end] == [1, 2, 3]
 
-cell = SpiceCharCell(3, 3)
-appnd("blob", cell)
-@test cell[1] == "blo"
-appnd("abc", cell)
-@test cell[1:end] == ["blo", "abc"]
+    cell = SpiceDoubleCell(3)
+    appnd(1, cell)
+    appnd(2, cell)
+    appnd(3, cell)
+    @test cell[1:end] == [1.0, 2.0, 3.0]
+
+    cell = SpiceDoubleCell(3)
+    push!(cell, 1, 2, 3)
+    @test cell[1:end] == [1.0, 2.0, 3.0]
+
+    cell = SpiceDoubleCell(3)
+    append!(cell, [1, 2, 3])
+    @test cell[1:end] == [1.0, 2.0, 3.0]
+
+    cell = SpiceCharCell(3, 6)
+    appnd("foobar1", cell)
+    appnd("foobar2", cell)
+    appnd("foobar3", cell)
+    @test cell[1:end] == fill("foobar", 3)
+
+    cell = SpiceCharCell(3, 6)
+    push!(cell, "foobar1", "foobar2", "foobar3")
+    @test cell[1:end] == fill("foobar", 3)
+
+    cell = SpiceCharCell(3, 6)
+    append!(cell, ["foobar1", "foobar2", "foobar3"])
+    @test cell[1:end] == fill("foobar", 3)
+
+    cell = SpiceIntCell(1)
+    @test_throws ErrorException cell[1]
+    @test_throws ErrorException cell[2]
+end
