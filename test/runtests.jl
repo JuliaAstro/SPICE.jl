47db8e57b92f6fdde646acd247c34792f04f51f4
Helge Eichhorn <git@helgeeichhorn.de>
2017-02-17 16:53:30 +0100

    Implement axisar.

--- test/runtests.jl
+++ test/runtests.jl
@@ -74,3 +74,10 @@ end
     @test_throws ErrorException cell[1]
     @test_throws ErrorException cell[2]
 end
+
+@testset "A" begin
+    axis = [0.0, 0.0, 1.0]
+    output = axisar(axis, pi/2)
+    expected = [0.0 -1.0 0.0; 1.0 0.0 0.0; 0.0 0.0 1.0]
+    @test output ≈ expected
+end
