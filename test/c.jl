9a5cf7ce85c1d1317ec269274aa52c4b84dfcfe4
Helge Eichhorn <git@helgeeichhorn.de>
2017-11-15 07:12:43 +0100

    Fix tests on Windows

--- test/c.jl
+++ test/c.jl
@@ -5,12 +5,15 @@
     @test cidfrm(399) == (10013, "IAU_EARTH")
     @test_throws SpiceException cidfrm(999999)
 
-    let name = tempname()
-        handle = ckopn(name)
+    file = "test.ck"
+    try
+        handle = ckopn(file)
         quats = reshape(collect(1.0:8.0), 4, 2)
         avvs = reshape(collect(1.0:6.0), 3, 2)
         sclkdp = [1.0, 2.0]
         ckw01(handle, -7701, "J2000", "GLL SCAN PLT - DATA TYPE 1", sclkdp, quats, avvs)
         ckcls(handle)
+    finally
+        rm(file, force=true)
     end
 end
