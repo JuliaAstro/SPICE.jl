776bd9b3cbd144d959fb524d80e303979440378a
Adriaen Van Camp <adriaen.vancamp@ptscientists.com>
2018-11-23 11:04:57 +0100

    Add docs for q2m

--- src/q.jl
+++ src/q.jl
@@ -1,5 +1,22 @@
 export q2m
 
+"""
+    q2m(q...)
+
+Find the rotation matrix corresponding to a specified unit quaternion.
+
+### Arguments ###
+
+- `q`: A unit quaternion (as any kind of iterable with four elements)
+
+### Output ###
+
+A rotation matrix corresponding to `q`.
+
+### References ###
+
+- [NAIF Documentation](https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/q2m_c.html)
+"""
 q2m(q...) = q2m(collect(q))
     
 function q2m(q)
