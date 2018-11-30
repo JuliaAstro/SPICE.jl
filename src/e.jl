3db641eb2624ae7795185d94821203dd5d1092ae
Helge Eichhorn <git@helgeeichhorn.de>
2018-11-30 15:31:15 +0100

    Update e.jl

--- src/e.jl
+++ src/e.jl
@@ -38,7 +38,12 @@ to Calendar, Day-of-Year, or Julian Date format, UTC.
 ### Arguments ###
 
 - `et`: Input epoch, given in ephemeris seconds past J2000
-- `format`: Format of output epoch
+- `format`: Format of output epoch. It may be any of the following:
+    - `:C`: Calendar format, UTC
+    - `:D`: Day-of-Year format, UTC
+    - `:J`: Julian Date format, UTC
+    - `:ISOC`: ISO Calendar format, UTC
+    - `:ISOD`: ISO Day-of-Year format, UTC
 - `prec`: Digits of precision in fractional seconds or days
 
 ### Output ###
@@ -50,14 +55,12 @@ epoch, in the specified format.
 
 - [NAIF Documentation](https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/et2utc_c.html)
 """
-# WIP
 function et2utc(et, format, prec)
-    # this is a guess
-    lenout = 30 
+    lenout = 32
     utcstr = Array{UInt8}(undef, lenout)
     ccall((:et2utc_c, libcspice), Cvoid,
           (SpiceDouble, Cstring, SpiceInt, SpiceInt, Ptr{UInt8}),
-          et, format, prec, lenout, utcstr)
+          et, string(format), prec, lenout, utcstr)
     handleerror()
     unsafe_string(pointer(utcstr))
 end
