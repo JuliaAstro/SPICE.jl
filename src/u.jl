0aa279f62111684daba24d183ed1d2087d3f2b3a
Helge Eichhorn <git@helgeeichhorn.de>
2018-12-06 10:40:48 +0100

    Add uddf

--- src/u.jl
+++ src/u.jl
@@ -2,6 +2,7 @@ import LinearAlgebra
 
 export ucase,
     ucrss,
+    uddf,
     unload
 
 function _ucase(in)
@@ -37,6 +38,41 @@ end
 ucrss
 
 @deprecate ucrss(v1, v2) LinearAlgebra.normalize(LinearAlgebra.cross(v1, v2))
+
+"""
+    uddf(udfunc, x, dx)
+
+Routine to calculate the first derivative of a caller-specified function using
+a three-point estimation.
+
+### Arguments ###
+
+- `udfunc`: A callable that computes the scalar value of interest,
+    e.g. `f(x::Float64) -> Float64`.
+- `x`: Independent variable of 'udfunc'
+- `dx`: Interval from `x` for derivative calculation
+
+### Output ###
+
+Returns the approximate derivative of `udfunc` at `x`.
+
+### References ###
+
+- [NAIF Documentation](https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/uddf_c.html)
+"""
+function uddf(udfunc, x, dx)
+    function _udfunc(et::SpiceDouble, value::Ptr{SpiceDouble})
+        value = unsafe_wrap(Array, value, 1)
+        value[1] = udfunc(et)
+        nothing
+    end
+    func = @cfunction($_udfunc, Cvoid, (SpiceDouble, Ptr{SpiceDouble}))
+    deriv = Ref{SpiceDouble}()
+    ccall((:uddf_c, libcspice), Cvoid,
+          (Ptr{Cvoid}, SpiceDouble, SpiceDouble, Ref{SpiceDouble}),
+          func, x, dx, deriv)
+    deriv[]
+end
 
 function unload(kernel)
     ccall((:unload_c, libcspice), Cvoid, (Cstring,), kernel)
--- test/u.jl
+++ test/u.jl
@@ -1,8 +1,28 @@
 import LinearAlgebra
 
 @testset "U" begin
-    @test SPICE._ucase("test") == uppercase("test")
-    let v1 = randn(3), v2 = randn(3)
+    @testset "ucase" begin
+        @test SPICE._ucase("test") == uppercase("test")
+    end
+    @testset "ucrss" begin
+        v1 = randn(3)
+        v2 = randn(3)
         @test SPICE._ucrss(v1, v2) ≈ LinearAlgebra.normalize(LinearAlgebra.cross(v1, v2))
+    end
+    @testset "uddf" begin
+        try
+            furnsh(path(CORE, :lsk), path(CORE, :spk))
+            et = str2et("JAN 1 2009")
+
+            function udfunc(et_in)
+                _, new_et = spkpos("MERCURY", et_in, "J2000", "MOON", abcorr="LT+S")
+                new_et
+            end
+
+            deriv = uddf(udfunc, et, 1.0)
+            @test deriv ≈ -0.000135670940 atol=1e-10
+        finally
+            kclear()
+        end
     end
 end
