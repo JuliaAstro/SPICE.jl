6b990a6597c4dbc3348c811e758f303776db573b
Helge Eichhorn <git@helgeeichhorn.de>
2019-02-08 07:07:45 +0100

    Fix surfpt docstring

--- src/s.jl
+++ src/s.jl
@@ -267,13 +267,25 @@ function subpnt(method, target, et, fixref, obsrvr; abcorr="NONE")
 end
 
 """
-positn     I   Position of the observer in body-fixed frame.
-u          I   Vector from the observer in some direction.
-a          I   Length of the ellipsoid semi-axis along the x-axis.
-b          I   Length of the ellipsoid semi-axis along the y-axis.
-c          I   Length of the ellipsoid semi-axis along the z-axis.
-point      O   Point on the ellipsoid pointed to by u.
-found      O   Flag indicating if u points at the ellipsoid.
+    surfpt(positn, u, a, b, c)
+
+Determine the intersection of a line-of-sight vector with the surface of an ellipsoid.
+
+### Arguments ###
+
+- `positn`: Position of the observer in body-fixed frame
+- `u`: Vector from the observer in some direction
+- `a`: Length of the ellipsoid semi-axis along the x-axis
+- `b`: Length of the ellipsoid semi-axis along the y-axis
+- `c`: Length of the ellipsoid semi-axis along the z-axis
+
+### Output ###
+
+Returns the point on the ellipsoid pointed to by u or `nothing` if none was found.
+
+### References ###
+
+- [NAIF Documentation](https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/surfpt_c.html)
 """
 function surfpt(positn, u, a, b, c)
     length(positn) != 3 && throw(ArgumentError("Length of `positn` must be 3."))
