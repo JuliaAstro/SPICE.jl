9b593b2b8128c70436b69085d54a63998abe05a6
Helge Eichhorn <git@helgeeichhorn.de>
2019-04-16 20:03:11 +0200

    Add stress testing macro

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,21 @@ using Test
 const BASE_URL = "https://raw.githubusercontent.com/AndrewAnnex/SpiceyPyTestKernels/master/"
 const KERNEL_DIR = joinpath(@__DIR__, "kernels")
 
+macro stress(n::Int, expr)
+    str = string(expr)
+    quote
+        print("Stress test (n=$($n)) for `", $str, "`:   0%")
+        GC.enable(false)
+        for i = 1:$n
+            print("\b\b\b\b", lpad(round(Int, i/$n * 100), 3), "%")
+            $expr
+        end
+        println()
+        GC.enable(true)
+        GC.gc()
+    end
+end
+
 @RemoteFileSet CASSINI "Cassini Kernels" begin
     pck      = @RemoteFile BASE_URL * "cpck05Mar2004.tpc" dir=KERNEL_DIR
     sat_spk  = @RemoteFile BASE_URL * "130220AP_SE_13043_13073.bsp" dir=KERNEL_DIR
@@ -121,29 +136,29 @@ download(CORE)
         end
     end
 
-    include("a.jl")
-    include("b.jl")
-    include("c.jl")
-    include("d.jl")
-    include("e.jl")
-    include("f.jl")
-    include("g.jl")
-    include("h.jl")
-    include("i.jl")
-    include("j.jl")
-    include("k.jl")
-    include("l.jl")
-    include("m.jl")
-    include("n.jl")
-    include("o.jl")
-    include("p.jl")
-    include("q.jl")
-    include("r.jl")
-    include("s.jl")
-    include("t.jl")
-    include("u.jl")
-    include("v.jl")
-    include("w.jl")
-    include("x.jl")
+    @stress 100 include("a.jl")
+    @stress 100 include("b.jl")
+    @stress 100 include("c.jl")
+    @stress 100 include("d.jl")
+    @stress 100 include("e.jl")
+    @stress 100 include("f.jl")
+    @stress 100 include("g.jl")
+    @stress 100 include("h.jl")
+    @stress 100 include("i.jl")
+    @stress 100 include("j.jl")
+    @stress 100 include("k.jl")
+    @stress 100 include("l.jl")
+    @stress 100 include("m.jl")
+    @stress 100 include("n.jl")
+    @stress 100 include("o.jl")
+    @stress 100 include("p.jl")
+    @stress 100 include("q.jl")
+    @stress 100 include("r.jl")
+    @stress 100 include("s.jl")
+    @stress 100 include("t.jl")
+    @stress 100 include("u.jl")
+    @stress 100 include("v.jl")
+    @stress 100 include("w.jl")
+    @stress 100 include("x.jl")
 end
 
