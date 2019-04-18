1af5d7ef928b7897a4bc45967b3bc0ea822f8d0f
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-04-18 10:17:06 +0000

    build based on 736de0e

--- dev/search_index.js
+++ dev/search_index.js
@@ -9,6 +9,46 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "#SPICE.jl-1",
+    "page": "Home",
+    "title": "SPICE.jl",
+    "category": "section",
+    "text": "SPICE.jl is a Julia wrapper for the SPICE toolkit which is provided by NASA\'s Navigation and Ancillary Information Facility (NAIF). It provides functionality to read SPICE data files and compute derived observation geometry such as altitude, latitude/longitude and lighting angles. Please refer to its comprehensive documentation if you are not yet familiar with SPICE.Note: This project is not affiliated with NASA, NAIF or JPL in any way."
+},
+
+{
+    "location": "#Installation-1",
+    "page": "Home",
+    "title": "Installation",
+    "category": "section",
+    "text": "julia> import Pkg; Pkg.add(\"SPICE\")"
+},
+
+{
+    "location": "#Quickstart-1",
+    "page": "Home",
+    "title": "Quickstart",
+    "category": "section",
+    "text": "# Download SPICE kernels from https://naif.jpl.nasa.gov/pub/naif/generic_kernels/\n\nusing SPICE\n\n# Load leap seconds kernel\nfurnsh(\"naif0012.tls\")\n\n# Convert the calendar date to ephemeris seconds past J2000\net = utc2et(\"2018-02-06T20:45:00\")\n\n# Load a planetary ephemeris kernel\nfurnsh(\"de430.bsp\")\n\n# Get the position of Mars at `et` w.r.t. Earth\nspkezp(\"mars\", et, \"J2000\", \"none\", \"earth\")"
+},
+
+{
+    "location": "#Notable-differences-to-CSPICE-1",
+    "page": "Home",
+    "title": "Notable differences to CSPICE",
+    "category": "section",
+    "text": "SPICE.jl follows the Julia language\'s style and conventions which introduces the following differences to CSPICE.SPICE.jl does not wrap all CSPICE functions since many general purpose utilities already exist in Julia\'s standard library or can be approximated with a Julia one-liner.  If you try to call an unwrapped function, SPICE.jl will return a deprecation warning that shows the equivalent Julia code.\nFunctions that mutate one of their input arguments are suffixed with ! and the to-be-mutated argument will be the first parameter, e.g. wninsd!(window, left, right) instead of wninsd(left, right, window)."
+},
+
+{
+    "location": "#Next-Steps-1",
+    "page": "Home",
+    "title": "Next Steps",
+    "category": "section",
+    "text": "Please refer to the API reference."
+},
+
+{
     "location": "api/#",
     "page": "API",
     "title": "API",
