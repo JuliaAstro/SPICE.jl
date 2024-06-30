03a5e726b878c253231b8311b2173d56c700a728
Alberto Fossà <alberto.fossa18@gmail.com>
2024-06-30 17:54:37 -0500

    wrapped azlcpo

--- src/a.jl
+++ src/a.jl
@@ -1,5 +1,6 @@
 export
     axisar,
+    azlcpo,
     azlrec
 
 """
@@ -25,6 +26,43 @@ function axisar(axis, angle)
     r = Matrix{Float64}(undef, 3,3)
     ccall((:axisar_c, libcspice), Cvoid, (Ref{SpiceDouble}, SpiceDouble, Ref{SpiceDouble}), axis, angle, r)
     permutedims(r)
+end
+
+"""
+    azlcpo(method, target, et, abcorr, azccw, elplsz, obspos, obsctr, obsref)
+
+Return the azimuth/elevation coordinates of a specified target relative to an "observer," 
+where the observer has constant position in a specified reference frame. 
+The observer's position is provided by the calling program rather than by loaded SPK files.
+
+### Arguments ###
+
+- `method`: Method to obtain the surface normal vector. The only choice currently supported is `"ELLIPSOID"`
+- `target`: Name of target ephemeris object
+- `et`: Observation epoch
+- `abcorr`: Aberration correction
+- `azccw`: Flag indicating how azimuth is measured. If `true`, the azimuth increases in the counterclockwise direction; otherwise it increases in the clockwise direction
+- `elplsz`: Flag indicating how elevation is measured. If `true`, the elevation increases from the XY plane toward +Z; otherwise toward -Z
+- `obspos`: Observer position relative to center of motion
+- `obsctr`: Center of motion of observer
+- `obsref`: Body-fixed, body-centered frame of observer's center
+
+### Output ###
+
+- `azlsta`: State of target with respect to observer, in azimuth/elevation coordinates: `azlsta = [r, az, el, dr/dt, daz/dt, del/dt]`
+- `lt`: One-way light time between target and observer
+
+### References ###
+
+- [NAIF Documentation](https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/azlcpo_c.html)
+"""
+function azlcpo(method, target, et, abcorr, azccw, elplsz, obspos, obsctr, obsref)
+    @checkdims 3 obspos
+    azlsta = Array{SpiceDouble}(undef, 6)
+    lt = Ref{SpiceDouble}()
+    ccall((:azlcpo_c, libcspice), Cvoid, (Cstring, Cstring, SpiceDouble, Cstring, SpiceBoolean, SpiceBoolean, Ref{SpiceDouble}, Cstring, Cstring, Ref{SpiceDouble}, Ref{SpiceDouble}), 
+        method, target, et, abcorr, azccw, elplsz, obspos, obsctr, obsref, azlsta, lt)
+    azlsta, lt[]
 end
 
 """
