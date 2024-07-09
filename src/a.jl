9f6f1f93d3e536f9c012ec174481ac6d77a3f7a2
Alberto Fossà <49210322+afossa@users.noreply.github.com>
2024-07-09 00:49:40 -0500

    Azlcpo wrapper (#28)
    * wrapped azlcpo
    
    * error handling in azlcpo
    
    * make azccw, elplsz and method optional kwargs

--- src/a.jl
+++ src/a.jl
@@ -1,5 +1,6 @@
 export
     axisar,
+    azlcpo,
     azlrec
 
 """
@@ -28,7 +29,48 @@ function axisar(axis, angle)
 end
 
 """
-    azlrec(range, az, el, azccw, elplsz)
+    azlcpo(target, et, abcorr, obspos, obsctr, obsref; method="ELLIPSOID", azccw=true, elplsz=true)
+
+Return the azimuth/elevation coordinates of a specified target relative to an "observer," 
+where the observer has constant position in a specified reference frame. 
+The observer's position is provided by the calling program rather than by loaded SPK files.
+
+### Arguments ###
+
+- `target`: Name of target ephemeris object
+- `et`: Observation epoch
+- `abcorr`: Aberration correction
+- `obspos`: Observer position relative to center of motion
+- `obsctr`: Center of motion of observer
+- `obsref`: Body-fixed, body-centered frame of observer's center
+
+### Keyword Arguments ###
+
+- `method`: Method to obtain the surface normal vector. The only (and default) choice currently supported is `"ELLIPSOID"`
+- `azccw`: Flag indicating how azimuth is measured. If `true` (the default), the azimuth increases in the counterclockwise direction; otherwise it increases in the clockwise direction
+- `elplsz`: Flag indicating how elevation is measured. If `true` (the default), the elevation increases from the XY plane toward +Z; otherwise toward -Z
+
+### Output ###
+
+- `azlsta`: State of target with respect to observer, in azimuth/elevation coordinates: `azlsta = [r, az, el, dr/dt, daz/dt, del/dt]`
+- `lt`: One-way light time between target and observer
+
+### References ###
+
+- [NAIF Documentation](https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/azlcpo_c.html)
+"""
+function azlcpo(target, et, abcorr, obspos, obsctr, obsref; method="ELLIPSOID", azccw=true, elplsz=true)
+    @checkdims 3 obspos
+    azlsta = Array{SpiceDouble}(undef, 6)
+    lt = Ref{SpiceDouble}()
+    ccall((:azlcpo_c, libcspice), Cvoid, (Cstring, Cstring, SpiceDouble, Cstring, SpiceBoolean, SpiceBoolean, Ref{SpiceDouble}, Cstring, Cstring, Ref{SpiceDouble}, Ref{SpiceDouble}), 
+        method, target, et, abcorr, azccw, elplsz, obspos, obsctr, obsref, azlsta, lt)
+    handleerror()
+    azlsta, lt[]
+end
+
+"""
+    azlrec(range, az, el; azccw=true, elplsz=true)
 
 Convert from range, azimuth and elevation of a point to rectangular coordinates.
 
@@ -37,8 +79,11 @@ Convert from range, azimuth and elevation of a point to rectangular coordinates.
 - `range`: Distance of the point from the origin
 - `az`: Azimuth in radians
 - `el`: Elevation in radians
-- `azccw`: Flag indicating how azimuth is measured. If `true`, the azimuth increases in the counterclockwise direction; otherwise it increases in the clockwise direction
-- `elplsz`: Flag indicating how elevation is measured. If `true`, the elevation increases from the XY plane toward +Z; otherwise toward -Z
+
+### Keyword Arguments ###
+
+- `azccw`: Flag indicating how azimuth is measured. If `true` (the default), the azimuth increases in the counterclockwise direction; otherwise it increases in the clockwise direction
+- `elplsz`: Flag indicating how elevation is measured. If `true` (the default), the elevation increases from the XY plane toward +Z; otherwise toward -Z
 
 ### Output ###
 
@@ -48,7 +93,7 @@ Convert from range, azimuth and elevation of a point to rectangular coordinates.
 
 - [NAIF Documentation](https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/azlrec_c.html)
 """
-function azlrec(range, az, el, azccw, elplsz)
+function azlrec(range, az, el; azccw=true, elplsz=true)
     rectan = Array{SpiceDouble}(undef, 3)
     ccall((:azlrec_c, libcspice), Cvoid, (SpiceDouble, SpiceDouble, SpiceDouble, SpiceBoolean, SpiceBoolean, Ref{SpiceDouble}), 
         range, az, el, azccw, elplsz, rectan)
--- src/r.jl
+++ src/r.jl
@@ -112,15 +112,18 @@ function raxisa(matrix)
 end
 
 """
-    recazl(rectan, azccw, elplsz)
+    recazl(rectan; azccw=true, elplsz=true)
 
 Convert rectangular coordinates of a point to range, azimuth and elevation.
 
 ### Arguments ###
 
 - `rectan`: Rectangular coordinates of a point
-- `azccw`: Flag indicating how azimuth is measured. If `true`, the azimuth increases in the counterclockwise direction; otherwise it increases in the clockwise direction
-- `elplsz`: Flag indicating how elevation is measured. If `true`, the elevation increases from the XY plane toward +Z; otherwise toward -Z
+
+### Keyword Arguments ###
+
+- `azccw`: Flag indicating how azimuth is measured. If `true` (the default), the azimuth increases in the counterclockwise direction; otherwise it increases in the clockwise direction
+- `elplsz`: Flag indicating how elevation is measured. If `true` (the default), the elevation increases from the XY plane toward +Z; otherwise toward -Z
 
 ### Output ###
 
@@ -134,7 +137,7 @@ Returns a tuple consisting of:
 
 - [NAIF Documentation](https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/recazl_c.html)
 """
-function recazl(rectan, azccw, elplsz)
+function recazl(rectan; azccw=true, elplsz=true)
     @checkdims 3 rectan
     range = Ref{SpiceDouble}()
     az = Ref{SpiceDouble}()
